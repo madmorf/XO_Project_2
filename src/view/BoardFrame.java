028a73d681c253d97ae601ec74975a35bebda6f7
madmorf <trihleb@gmail.com>
2016-03-24 20:58:18 +0200

    BoardFrame first config

---
+++ src/view/BoardFrame.java
@@ -0,0 +1,26 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BoardFrame extends JFrame  {
+    private Toolkit toolkit = Toolkit.getDefaultToolkit();
+
+    public BoardFrame(String title){
+        super();
+        configure();
+    }
+
+    private void configure(){
+        setResizable(false);
+        putFrameInTheCenterOfTheScreen();
+    }
+
+    private void putFrameInTheCenterOfTheScreen() {
+        Dimension screenSize = toolkit.getScreenSize();
+
+        setLocation((screenSize.width - getWidth()) / 2, (screenSize.height - getHeight()) / 2);
+    }
+
+
+}
