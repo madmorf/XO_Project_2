2f5edb76f348a3d3399c3d621b6c5747609514ef
madmorf <trihleb@gmail.com>
2016-03-24 21:05:50 +0200

    Player configured

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,4 +1,20 @@
 package model;
 
 public class Player {
+
+    private String Name;
+    private Figure figure;
+
+    public Player(String name, Figure figure) {
+        Name = name;
+        this.figure = figure;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
 }
