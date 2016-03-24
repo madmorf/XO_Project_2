7f6d317ea395987d99a508362886c94c94db53c4
madmorf <trihleb@gmail.com>
2016-03-24 20:57:22 +0200

    Start - build main window

--- src/controller/Start.java
+++ src/controller/Start.java
@@ -1,7 +1,22 @@
 package controller;
 
+import view.BoardFrame;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Start {
     public static void main(String[] args) {
+
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                BoardFrame BoardWiev = new BoardFrame("XO GAME");
+                BoardWiev.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                BoardWiev.setVisible(true);
+            }
+        });
+
 
     }
 }
