e8bcc3420ba1d19d47a9bf0f2b6b80cbae4440db
madmorf <trihleb@gmail.com>
2016-03-25 10:06:59 +0200

    Core configurated

--- src/controller/Core.java
+++ src/controller/Core.java
@@ -1,4 +1,19 @@
 package controller;
 
+import model.Board;
+import model.Figure;
+import model.Player;
+
+import java.awt.*;
+
 public class Core {
+
+    private Figure[][] field;
+
+    public void setFogureToTheField(Board board, Point coordinate, Player player){
+        field = board.getGameField();
+        field[(int) coordinate.getX()][(int) coordinate.getY()] = player.getFigure();
+
+    }
+
 }
