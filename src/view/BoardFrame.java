011fbfc8cb824e178a7ab3c27ff5c0e6a6bbc96c
madmorf <trihleb@gmail.com>
2016-03-25 19:26:29 +0200

    Configure BoardFrame

--- src/view/BoardFrame.java
+++ src/view/BoardFrame.java
@@ -1,28 +1,40 @@
 package view;
 
+import controller.GameKernell;
+import model.*;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class BoardFrame extends JFrame  {
+public class BoardFrame extends JFrame {
     private Toolkit toolkit = Toolkit.getDefaultToolkit();
+    private GameKernell gameGameKernell = new GameKernell();
 
-    public BoardFrame(String title){
-        super();
-        configure();
+    public BoardFrame(String title) {
         setTitle(title);
+        configure();
+        addButtons();
     }
 
-    private void configure(){
-        setResizable(false);
-        setSize(400,400);
+    private void configure() {
+        setResizable(true);
+        setSize(new Dimension(400,400));
         putFrameInTheCenterOfTheScreen();
+        setLayout(new GridLayout(Board.getFieldHeight(),Board.getFieldWidth()));
     }
 
     private void putFrameInTheCenterOfTheScreen() {
         Dimension screenSize = toolkit.getScreenSize();
-
         setLocation((screenSize.width - getWidth()) / 2, (screenSize.height - getHeight()) / 2);
     }
 
+    private void addButtons(){
+        for (int i = 0; i < Board.getFieldHeight(); i++){
+            for (int j = 0; j < Board.getFieldWidth(); j++){
+                add(new model.Button(new Point(i,j), gameGameKernell));
+
+            }
+        }
+    }
 
 }
