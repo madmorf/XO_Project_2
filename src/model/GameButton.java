03a2ae063086cf1c28533a56464e6d4448c0f4a9
madmorf <trihleb@gmail.com>
2016-03-27 21:19:58 +0300

    Add configure, Change Icons methods

--- src/model/GameButton.java
+++ src/model/GameButton.java
@@ -2,28 +2,59 @@ package model;
 
 
 import controller.GameKernell;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GameButton extends JButton {
+
     private Point coordinatePoint;
 
-    public GameButton(Point coordinatePoint, GameKernell gameGameKernell) {
+    private GameKernell gameKernell;
+
+    public GameButton(Point coordinatePoint, GameKernell kernell) {
         this.coordinatePoint = coordinatePoint;
-        setIcon(new ImageIcon("/home/alex/dev/ide/IntellijIdea/XO_Project/resources/empty.png"));
-        setText(coordinatePoint.toString());
-        setBorder(BorderFactory.createLoweredSoftBevelBorder());
+        this.gameKernell = kernell;
+        configureButton();
+        addListener();
+    }
+
+    private void addListener() {
         addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            gameGameKernell.setFigureToTheField(getCoordinatePoint(), gameGameKernell.getNextPlayer());
-
+                changeIcon();
+                gameKernell.setFigureToTheField(getCoordinatePoint(), gameKernell.getNextPlayer());
             }
         });
     }
 
+    private void configureButton() {
+        setBorder(BorderFactory.createLoweredSoftBevelBorder());
+        try {
+            setIcon(new ImageIcon(getClass().getResource("Resources/Empty.png")));
+        } catch (Exception e) {
+        }
+    }
+
+    private void changeIcon() {
+        if (gameKernell.getNextPlayer() == gameKernell.getPlayerX()) {
+            try {
+                setIcon(new ImageIcon(getClass().getResource("Resources/X.png")));
+                setHorizontalTextPosition(AbstractButton.CENTER);
+            } catch (Exception e) {
+            }
+        } else {
+            try {
+                setIcon(new ImageIcon(getClass().getResource("Resources/O.png")));
+                setHorizontalTextPosition(AbstractButton.CENTER);
+            } catch (Exception e) {
+            }
+        }
+
+    }
 
     public Point getCoordinatePoint() {
         return coordinatePoint;
